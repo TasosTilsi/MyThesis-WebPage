f40cc8fd582f39d739221da10a49835a8a20b9db
TasosTilsi <tasostilsi@hotmail.com>
2018-03-02 16:42:52 +0200

    Generate Numbers is ready, table view pending

--- GenerateRandomNumbers.js
+++ GenerateRandomNumbers.js
@@ -1,44 +1,45 @@
 const DEFAULT_ARRAY_SIZE = 100;
 const DEFAULT_NUMBERS_RANGE = 100;
 
-function generateRandomNumbers(size, range) {
-    for (var i = 0; i < size; i++) {
-        generatedNumbers[i] = Math.floor(Math.random() * range + 1);
-    }
-    console.log(size)
-    return size;
+function generateRandomNumbers(arraySize, numbersRange) {
+	var generatedNumbers = [];
+	for (var i = 0; i < arraySize; i++) {
+		generatedNumbers[i] = Math.floor(Math.random() * numbersRange + 1);
+	}
+	return generatedNumbers;
 }
 
-function displayArray(generateRandomNumbers) {
-    canvas = document.getElementById("canvas")
-    var context = canvas.getContext("2d")
-    context.font = "20px Georgia";
-    console.log(generateRandomNumbers.length)
-    for (var i = 0; i < generateRandomNumbers.length; i++) {
-        console.log(generateRandomNumbers[i]);
-        context.fillText(generateRandomNumbers[i], i * 30, 50); //Be smarter here to control where text displays
-
-    }
+function displayArray() {
+	var new_array;
+	var array_size = document.getElementById("arraySize").value;
+	array_size = array_size=="" ? DEFAULT_ARRAY_SIZE : parseInt(array_size);
+	var numbers_range = document.getElementById("numbersRange").value;
+	numbers_range = numbers_range=="" ? DEFAULT_NUMBERS_RANGE : parseInt(numbers_range);
+	new_array = generateRandomNumbers(array_size, numbers_range);
+	draw(new_array,array_size,numbers_range);
+	return 0;
 }
 
-if (document.getElementById("arraySize").length > 0 && document.getElementById("numbersRange").length > 0) {
-    var new_array = Array(parseInt(document.getElementById("arraySize").value))
-    new_array = generateRandomNumbers(parseInt(document.getElementById("arraySize").value), parseInt(document.getElementById("numbersRange").value))
-} else if (document.getElementById("arraySize").length > 0) {
-    var new_array = Array(parseInt(document.getElementById("arraySize").value))
-    new_array = generateRandomNumbers(parseInt(document.getElementById("arraySize").value), DEFAULT_NUMBERS_RANGE)
-} else if (document.getElementById("numbersRange").length > 0) {
-    var new_array = Array(DEFAULT_ARRAY_SIZE)
-    new_array = generateRandomNumbers(DEFAULT_ARRAY_SIZE, parseInt(document.getElementById("numbersRange").value))
-} else {
-    var new_array = Array(DEFAULT_ARRAY_SIZE)
-    new_array = generateRandomNumbers(DEFAULT_ARRAY_SIZE, DEFAULT_NUMBERS_RANGE)
+function draw(new_array,array_size,numbers_range){
+	numbers_container = document.getElementById("numbers_container");
+	numbers_container.innerHTML = new_array;
+	table_container = document.getElementById("table_container");
+	
+	//var my_table = '<table class="table">';
+	var my_table = document.createElement("div");
+	my_table.setAttribute("class", "row");
+	for(var i = 0; i<array_size;i++){
+		var row = my_table.insertRow;
+		for(var k = 0;k<10;k++){
+			var col = row.insertCell;
+			col.innerHTML = array_size[i];
+		}
+
+	}
+	numbers_container.innerHTML = new_array;
+	console.log(new_array.length);
 }
 
-//document.getElementById("generateNumbers").addEventListener("click", displayArray(new_array));
+document.getElementById("generateNumbers").addEventListener("click", displayArray);
 
-function myAlert() {
-    alert("test" + generateRandomNumbers)
-}
-
-document.getElementById("generateNumbers").innerHTML.addEventListener("click", myAlert());
+//setInterval(function(){alert("adding listener"); document.body.addEventListener("click", myAlert);}, 2000)
