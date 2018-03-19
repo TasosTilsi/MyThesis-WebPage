0ccd97dd6982746f9a6a4dc188080315266da792
TasosTilsi <tasostilsi@hotmail.com>
2018-03-19 15:32:29 +0200

    jump search finds the right value,problem with return -1

--- GeneralFunctions.js
+++ GeneralFunctions.js
@@ -2,6 +2,7 @@ function makeTheTableWhite() {
     for (var i = 0; i < the_array.length; i++) {
         let cell = document.querySelector("[cell_id='" + i + "']").style.backgroundColor;
         if (cell.length > 0 && cell != "white") {
+            console.log("Clearing "+i+" cell");
             document.querySelector("[cell_id='" + i + "']").style.backgroundColor = "white";
         }
     }
--- JumpSearch.js
+++ JumpSearch.js
@@ -1,11 +1,9 @@
-var handle;
-
 function checkForGeneratedNumbers() {
     if (document.getElementsByClassName("index").item(0) != null) {
         makeTheTableWhite();
         var searching_number = parseInt(document.getElementById("searchingNumber").value);
         if (!isNaN(searching_number)) {
-            handle = setInterval(jumpSearch(the_array, searching_number), 750);
+            var position = jumpSearch(the_array, searching_number);
         } else {
             showSnackBar("Please <strong>Specify a Number</strong> for search");
         }
@@ -14,55 +12,12 @@ function checkForGeneratedNumbers() {
     }
 }
 
-function jumpSearch(arrayToSearch, valueToSearch) {
-    let length = arrayToSearch.length;
-    let step = Math.floor(Math.sqrt(length));
-    document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "orange";
-    let index = Math.min(step, length) - 1;
-    let lowerBound = 0;
-    document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "orange";
-    while (arrayToSearch[Math.min(step, length) - 1] < valueToSearch) {
-        document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "white";
-        lowerBound = step;
-        document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "orange";
-        document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "white";
-        step += step;
-        document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "orange";
-        if (lowerBound >= length) {
-            clearInterval(handle);
-            showSnackBar("The number you searched for is not in the generated array!");
-            return -1;
-        }
-    }
-
-    var upperBound = Math.min(step, length);
-    while (arrayToSearch[lowerBound] < valueToSearch) {
-        document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "white";
-        lowerBound++;
-        document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "orange";
-        if (lowerBound == upperBound) {
-            clearInterval(handle);
-            showSnackBar("The number you searched for is not in the generated array!");
-            return -1;
-        }
-    }
-    if (arrayToSearch[lowerBound] == valueToSearch) {
-        clearInterval(handle);
-        document.querySelector("[cell_id='" + (lowerBound) + "']").style.backgroundColor = "lightgreen";
-        showSnackBar("The number you searched found in position " + lowerBound);
-        return lowerBound;
-    }
-    clearInterval(handle);
-    showSnackBar("The number you searched for is not in the generated array!");
-    return -1;
-}
-
-/*function jumpSearch(searching_array, asked_number) {
+function jumpSearch(searching_array, asked_number) {
     //Finding the array size
     let size = searching_array.length;
     // Finding block size to be jumped
     let step = Math.floor(Math.sqrt(size));
-    document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "orange";
+    document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "lightblue";
 
     // Finding the block where element is present (if it is present)
     let previous = 0;
@@ -74,39 +29,41 @@ function jumpSearch(arrayToSearch, valueToSearch) {
             previous = step;
             document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "orange";
 
-            document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "white";
-            step += Math.floor(Math.sqrt(size));
-            document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "orange";
-
-            if (previous >= size) {
+            if (step < size) {
+                document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "white";
+                step += Math.floor(Math.sqrt(size));
+                document.querySelector("[cell_id='" + (step) + "']").style.backgroundColor = "lightblue";
+            }
+            if (previous > size) {
                 clearInterval(handle);
+                console.log("The number you searched for is not in the generated array!");
                 showSnackBar("The number you searched for is not in the generated array!");
                 return -1;
             }
         }
 
-        if (searching_array[previous] < asked_number) {
-            document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "white";
-            previous++;
-            document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "orange";
-            if (previous == Math.min(step, size)) {
-                clearInterval(handle);
-                showSnackBar("The number you searched for is not in the generated array!");
-                return -1
+        let LinearHandle = setInterval(function () {
+            if (searching_array[previous] < asked_number) {
+                document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "white";
+                previous++;
+                document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "orange";
+                if (previous == size) {
+                    clearInterval(handle);
+                    console.log("The number you searched for is not in the generated array!!!");
+                    showSnackBar("The number you searched for is not in the generated array!");
+                    return -1
+                }
             }
-        }
+        }, 500);
 
         if (searching_array[previous] == asked_number) {
             clearInterval(handle);
             document.querySelector("[cell_id='" + (previous) + "']").style.backgroundColor = "lightgreen";
+            console.log("The number you searched found in position " + previous);
             showSnackBar("The number you searched found in position " + previous);
             return previous;
         }
-
-        clearInterval(handle);
-        showSnackBar("The number you searched for is not in the generated array!");
-        return -1;
     }, 750);
-}*/
+}
 
 document.getElementById("jumpSearch").addEventListener("click", checkForGeneratedNumbers);
